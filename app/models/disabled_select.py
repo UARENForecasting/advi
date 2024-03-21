45c4c0a7b94f45d9dc1fb1c5c2f4f37cdf2101a7
Leland Boeman <lboeman@arizona.edu>
2024-03-21 10:35:34 -0700

    update models, remove unneeded

--- app/models/disabled_select.py
+++ app/models/disabled_select.py
@@ -1,25 +1,37 @@
+# Includes modified Select class found in version 3.3.2 in src file
+# /src/bokeh/models/widgets/input.py
 from bokeh.models.callbacks import Callback
-from bokeh.core.properties import (List, Either, String, Tuple, Bool,
-                                   Instance)
+from bokeh.core.properties import (
+	List,
+	Either,
+    String,
+    Tuple,
+    Bool,
+	Dict,
+    Instance,
+	Null
+)
 from bokeh.models.widgets.inputs import InputWidget
-
+from bokeh.util.compiler import TypeScript
 
 class DisabledSelect(InputWidget):
-    ''' Single-select widget.
+    ''' Single-select widget with capability to have disabled options.
+
     '''
-    __js_implementation__ = 'disabled_select.ts'
+    __implementation__ = 'disabled_select.ts'
+    # explicit __init__ to support Init signatures
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
     options = List(Either(String, Tuple(String, Bool)), help="""
     Available selection options. Options may be provided either as a list of
     possible string values, or as a list of tuples, each of the form
-    ``(value, disabled)``.
-    """)
+    ``(value, disabled)``. In the latter case, the visible widget text for each
+    value will be the value. Option groupings can be provided
+    by supplying a dictionary object whose values are in the aforementioned
+    list format
+    """).accepts(List(Either(Null, String)), lambda v: [ "" if item is None else item for item in v ])
 
     value = String(default="", help="""
     Initial or selected value.
-    """)
-
-    callback = Instance(Callback, help="""
-    A callback to run in the browser whenever the current Select dropdown
-    value changes.
-    """)
+    """).accepts(Null, lambda _: "")
